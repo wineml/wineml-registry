470d6e722053e485f037d3abb1b888672bc17c28
Melvin Low <la0bing07148@gmail.com>
2023-06-29 20:44:30 +0800

    feat: postgres db support

--- wineml_registry/service/src/db/__init__.py
+++ wineml_registry/service/src/db/__init__.py
@@ -8,7 +8,15 @@ def db_resolver():
     Check whether db is local or postgres (or mysql?)
     """
 
-    if False:
+    if all(
+        [
+            os.getenv("POSTGRES_USER"),
+            os.getenv("POSTGRES_PASSWORD"),
+            os.getenv("POSTGRES_HOST"),
+            os.getenv("POSTGRES_PORT"),
+            os.getenv("POSTGRES_DB"),
+        ]
+    ):
         return "postgres"
     else:
         return "local"
@@ -22,7 +30,15 @@ def initiate_db():
     db_connector = db_resolver()
 
     if db_connector == "postgres":
-        ...
+        from db.postgres import PostgresModelConnector
+
+        return PostgresModelConnector(
+            username=os.environ["POSTGRES_USER"],
+            password=os.environ["POSTGRES_PASSWORD"],
+            host=os.environ["POSTGRES_HOST"],
+            port=os.environ["POSTGRES_PORT"],
+            database=os.environ["POSTGRES_DB"],
+        )
     elif db_connector == "mysql":
         ...
     elif db_connector == "local":
